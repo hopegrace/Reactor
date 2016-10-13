5bb1f3db6243687b6a9e3e84832e8630ff92e998
sduzh <zhuming9011@qq.com>
2016-10-13 23:11:25 +0800

    use getaddrinfo(3) to resolve inet address

--- reactor/net/InetAddress.cc
+++ reactor/net/InetAddress.cc
@@ -1,11 +1,44 @@
 #include "InetAddress.h"
+#include <string.h>
 #include <strings.h>
 #include <sstream>
 #include <arpa/inet.h>
+#include <sys/types.h>
+#include <netdb.h>
+#include <reactor/base/SimpleLogger.h>
 
 using namespace std;
 
 namespace reactor {
+
+namespace detail {
+
+static bool resolve_addr(const std::string &host, const std::string &port, struct sockaddr_in *res) {
+	struct addrinfo hints;
+	struct addrinfo *result = NULL;
+	::bzero(&hints, sizeof hints);
+	hints.ai_family = AF_INET;
+	hints.ai_socktype = SOCK_STREAM;
+	
+	int ret = ::getaddrinfo(&*host.begin(), &*port.begin(), &hints, &result);
+	if (ret == 0) {
+		for (struct addrinfo *p = result; p; p = p->ai_next) {
+			if (p->ai_family == AF_INET && p->ai_socktype == SOCK_STREAM 
+					&& p->ai_addrlen == sizeof(struct sockaddr_in)) {
+				*res = *reinterpret_cast<struct sockaddr_in*>(p->ai_addr);
+			}
+		}
+	} else {
+		LOG(Error) << ::strerror(errno);
+	}
+
+	::freeaddrinfo(result);
+
+	return ret == 0;
+}
+
+} 
+
 namespace net {
 
 typedef InetAddress::string_t string_t;
@@ -16,11 +49,21 @@ InetAddress::InetAddress(): valid_(false), host_(), port_() {
 
 InetAddress::InetAddress(const string_t &host, uint16_t port) : 
 		valid_(false), host_(host), port_(port) {
-	bzero(&sockaddr_, sizeof sockaddr_);
-	sockaddr_.sin_family = AF_INET;
-	sockaddr_.sin_port = htons(port);
-	int ret = inet_pton(AF_INET, host.c_str(), &sockaddr_.sin_addr);
-	valid_ = (ret == 0);
+	//bzero(&sockaddr_, sizeof sockaddr_);
+	//sockaddr_.sin_family = AF_INET;
+	//sockaddr_.sin_port = htons(port);
+	//int ret = inet_pton(AF_INET, host.c_str(), &sockaddr_.sin_addr);
+	//valid_ = (ret == 0);
+	//if (!valid_) {
+	//	fprintf(stderr, "invalid address: %s %d\n", host.c_str(), port);
+	//}
+	struct sockaddr_in addr;
+	char str_port[6];
+	snprintf(str_port, 6, "%u", port);
+	::bzero(&addr, sizeof addr);
+	int ret = detail::resolve_addr(host, str_port, &addr);	
+	set_address(addr);
+	valid_ = ret;
 }
 
 InetAddress::InetAddress(const struct sockaddr_in &addr) :
